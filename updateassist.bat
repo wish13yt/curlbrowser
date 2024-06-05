7b6c4a1736e4b91b3a7f4bbd0d1ec7157f8a0150
Wish <wishdog2013@hotmail.com>
2024-06-05 16:09:39 -0500

    Delete updateassist.bat

--- updateassist.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Updating...
-set $url=http://dl.dropbox.com/u/6227707/Scripts/new.bat
-curl -f -s -o "%~n0.tmp" "%$url%"&& (
-fc /B "%~n0.tmp" "%~0" >nul|| (copy /y "%~n0.tmp" "%~0"&& "%~0"))
-echo Update Successful!
-pause
-exit
