c659c34bc10119e29164a12cd01f3c6245889118
Wish <wishdog2013@hotmail.com>
2024-08-01 15:20:31 -0500

    Add files via upload

--- curlbrowser.bat
+++ curlbrowser.bat
@@ -9,7 +9,7 @@ echo Parrot
 echo Shell Guide for Linux
 echo Weather
 echo Browser Info (Self-Serve Page)
-echo News (Being worked on)
+echo Homebrew (Wii U)
 pause
 echo Curl Browser may experience bugs. Please report any issues on the Itch.io page or GitHub.
 echo Some sites may open in a new window. Please check your taskbar before reporting any issues.
@@ -29,4 +29,23 @@ if "%site%" == "Weather" (
 )
 if "%site%" == "Browser Info" (
 FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v ProductName`) DO echo OS: %%i %%j %%k
+)
+if "%site%" == "Homebrew" (
+echo This will download from https://hb-app.store/
+echo Files will be saved to the directory which this is being run from.
+echo WARNING! There is always a chance for a brick. I am not responsible if you screw up your console somehow!
+echo By unpausing, you have read this fully and agree.
+pause
+echo What's the homebrew you want to download?
+set /p brew=
+echo What's your Windows username?
+set /p user=
+cd C:\Users\%user%\Downloads\
+curl -O https://wiiu.cdn.fortheusers.org/zips/%brew%.zip
+echo Operation successful. Please follow the guide for your requested software.
+pause
+)
+if "%site%" == "hej" (
+echo https://tenor.com/view/android-dance-androiddance-hej-hi-gif-2340933326644126256
+pause
 )
